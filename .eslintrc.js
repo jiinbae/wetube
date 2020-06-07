d20fcbd7fc56d14ef4898b5b62b0c3de895e88ed
jiinbae <jiinbae98@gmail.com>
2020-06-08 01:35:34 +0900

    Installing ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,5 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": [
-        "airbnb-base", "plugin:prettier/recommended"],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 11,
-        "sourceType": "module"
-    },
+    "extends": [ "airbnb-base", "plugin:prettier/recommended"],
     "rules": {
         "no-console": "off"
     }
