233f53f72577487c6f73bad739161d08d06b56be
jiinbae <jiinbae98@gmail.com>
2020-06-09 11:52:13 +0900

    User Detail + Facebook Login Part One

--- passport.js
+++ passport.js
@@ -1,7 +1,11 @@
 import passport from "passport";
 import GithubStrategy from "passport-github";
 import User from "./models/User.js";
-import { githubLoginCallback } from "./controllers/userController.js";
+import {
+    githubLoginCallback,
+    facebookLoginCallback
+} from "./controllers/userController.js";
+
 passport.use(User.createStrategy());
 
 passport.use(
@@ -11,6 +15,17 @@ passport.use(
         callbackURL: `http://localhost:4000${routes.githubcallback}`
     },
     githubLoginCallback
+    )
+);
+
+passport.use(
+    new FacebookStrategy(
+      {
+        clientID: process.env.FB_ID,
+        clientSecret: process.env.FB_SECRET,
+        callbackURL: `http://localhost:4000${routes.facebookCallback}`
+      },
+      facebookLoginCallback
     )
 );
 
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -10,7 +10,9 @@ import {
     postLogin,
     githubLogin,
     postGithubLogIn,
-    getMe
+    getMe,
+    facebookLogin,
+    postFacebookLogin
   } from "../controllers/userController.js";
 import { onlyPrivate, onlyPublic } from "../middlewares.js";
 import passport from "passport";
@@ -35,5 +37,12 @@ globalRouter.get(
 );
 
 globalRouter.get(routes.me, getMe);
+
+globalRouter.get(routes.facebook, facebookLogin);
+globalRouter.get(
+   routes.facebookCallback,
+   passport.authenticate("facebook", { failureRedirect: "/login" }),
+   postFacebookLogin
+);
 
 export default globalRouter;
